1cbe79328fdc56ff32d296180f192f6c76d08a22
Jimmy Le <jimmyle414@gmail.com>
2020-03-17 17:56:20 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(sentence1)
+  sentence_new = []
+  sentence1.each do |sentence|
+    sentence_new << ("Hello there, and how are you?").reverse
+  end
+  sentence_new
+end
+
+  
