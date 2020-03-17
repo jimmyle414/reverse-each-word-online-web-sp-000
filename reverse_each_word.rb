1a1b4e149d3d2daf977e5491ee9de44338615993
Jimmy Le <jimmyle414@gmail.com>
2020-03-17 18:14:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,8 +12,7 @@ def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   new1_array = []
   sentence_array.collect do |sentence| sentence.reverse
-   
-new1_array.join(" ")
+  new1_array.join(" ")
 end
   
   
